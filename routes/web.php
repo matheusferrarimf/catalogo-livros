831027badf01bc22999ac10c92608bf327aba129
matheusferrarimf <matheusferrarimf@unidavi.edu.br>
2025-05-22 16:05:24 -0300

    alterações iniciais

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\LivroController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/livros');
 });
+
+Route::resource('autores', AutorController::class);
+Route::resource('livros', LivroController::class);
